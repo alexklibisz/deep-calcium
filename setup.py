4aebe86e3bfc45518906f93cd414da6052248b1d
Alex Klibisz <aklibisz@gmail.com>
2017-08-08 15:14:47 -0400

    Setup requirement fixes.

--- setup.py
+++ setup.py
@@ -2,7 +2,19 @@ from pip.req import parse_requirements
 from setuptools import setup
 from setuptools import find_packages
 
+# Some packages need to be installed directly from github.
+# tensorflow-gpu cannot be installed with the explicit version.
+
+
+def ignore_req(reqstr):
+    bad_reqs = ['c2s', 'cmt', 'tensorflow-gpu']
+    return bool(sum([reqstr.startswith(b) for b in bad_reqs]))
+
+
 install_reqs = parse_requirements('requirements.txt', session='hack')
+install_reqs = [str(ir.req) for ir in install_reqs
+                if not ignore_req(str(ir.req))]
+install_reqs.append('tensorflow-gpu')
 
 setup(name='deepcalcium',
       version='0.0.1',
@@ -10,12 +22,6 @@ setup(name='deepcalcium',
       author='Alex Klibisz',
       author_email='aklibisz@gmail.com',
       url='https://github.com/alexklibisz/deep-calcium',
-
-      # Had trouble installing the explicit version tensorflow-gpu==1.2.1
-      # from the requirements file, motivating the following fix.
-      install_requires=['tensorflow-gpu'] +
-          [str(ir.req) for ir in install_reqs
-           if 'tensorflow-gpu' not in str(ir.req)],
-
+      install_requires=install_reqs,
       extras_require={},
       packages=find_packages())
