bfc25466c72270198abe41d5a1e310eff2de2d73
Alex Klibisz <aklibisz@gmail.com>
2017-08-09 13:44:11 -0400

    Better get_config() function.

--- deepcalcium/utils/config.py
+++ deepcalcium/utils/config.py
@@ -11,24 +11,24 @@ def get_config():
     base_dir = base_dir_windows if system() == 'Windows' else base_dir_unix
     config_path = '%s/deep-calcium.json' % base_dir
 
-    config_obj = {
-        'datasets_dir': '%s/datasets' % base_dir,
-        'checkpoints_dir': '%s/checkpoints' % base_dir
-    }
-    os.makedirs(config_obj['datasets_dir'], exist_ok=True)
-    os.makedirs(config_obj['checkpoints_dir'], exist_ok=True)
-
     if not os.path.exists(base_dir):
         os.mkdir(base_dir)
 
     if not os.path.exists(config_path):
+        config = {
+            'datasets_dir': '%s/datasets' % base_dir,
+            'checkpoints_dir': '%s/checkpoints' % base_dir
+        }
         fp = open(config_path, 'w')
-        json.dump(config_obj, fp)
+        json.dump(config, fp)
+        fp.close()
+    else:
+        fp = open(config_path, 'r')
+        config = json.load(fp)
         fp.close()
 
-    fp = open(config_path, 'r')
-    config = json.load(fp)
-    fp.close()
+    os.makedirs(config['datasets_dir'], exist_ok=True)
+    os.makedirs(config['checkpoints_dir'], exist_ok=True)
     return config
 
 
